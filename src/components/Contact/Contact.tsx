b56b3686fa1199beb3b8eab645e16404329f489e
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2022-03-03 21:51:07 -0800

    emailjs

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Section } from "./contact.styles";
+import emailjs, { init } from "@emailjs/browser";
+import dotenv from "dotenv";
 
 function Contact() {
+  const form = useRef();
+
+  const emailjsServiceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  console.log(emailjsServiceId);
+  const emailjsTemplateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  // const emailjsUserId:string = process.env.REACT_APP_EMAILJS_USER_ID;
+
+  // init(emailjsUserId)
+
   const [isSubmit, setIsSubmit] = useState(false);
-  const [form, setForm] = useState({
+  const [_form, setForm] = useState({
     name: "",
     email: "",
     message: "",
@@ -14,50 +25,70 @@ function Contact() {
     message: false,
   });
 
-  const onChangName = (e: { target: { value: string } }) => {
-    setForm({ ...form, name: e.target.value });
-    setIsFilled({ ...isFilled, name: true });
-  };
+  // const onChangName = (e: { target: { value: string } }) => {
+  //   setForm({ ..._form, name: e.target.value });
+  //   setIsFilled({ ...isFilled, name: true });
+  // };
 
-  const onChangEmail = (e: { target: { value: string } }) => {
-    setForm({ ...form, email: e.target.value });
-    setIsFilled({ ...isFilled, email: true });
-  };
+  // const onChangEmail = (e: { target: { value: string } }) => {
+  //   setForm({ ..._form, email: e.target.value });
+  //   setIsFilled({ ...isFilled, email: true });
+  // };
 
-  const onChangMessage = (e: { target: { value: string } }) => {
-    setForm({ ...form, message: e.target.value });
-    setIsFilled({ ...isFilled, message: true });
-  };
+  // const onChangMessage = (e: { target: { value: string } }) => {
+  //   setForm({ ..._form, message: e.target.value });
+  //   setIsFilled({ ...isFilled, message: true });
+  // };
 
   const handleSubmit = (event: { preventDefault: () => void }) => {
     console.log(isFilled);
     event.preventDefault();
+    // emailjs
+    //   .sendForm(
+    //     emailjsServiceId,
+    //     emailjsTemplateId,
+    //     form.current,
+    //     "YOUR_USER_ID"
+    //   )
+    //   .then(
+    //     (result) => {
+    //       console.log(result.text);
+    //     },
+    //     (error) => {
+    //       console.log(error.text);
+    //     }
+    //   );
   };
 
   return (
     <Section id="contact">
       <h2>Contact</h2>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        // ref={form}
+      >
+        <label htmlFor="name">Name</label>
         <input
           type="text"
-          placeholder="Name *"
+          name="name"
           required={true}
-          onChange={onChangName}
+          // onChange={onChangName}
         />
+        <label htmlFor="name">Email</label>
         <input
           type="email"
-          placeholder="Email *"
+          name="email"
           required={true}
-          onChange={onChangEmail}
+          // onChange={onChangEmail}
         />
+        <label htmlFor="name">Message</label>
         <textarea
           name=""
           id=""
           cols={30}
           rows={10}
           required={true}
-          placeholder="Message *"
-          onChange={onChangMessage}
+          // onChange={onChangMessage}
         ></textarea>
         {isFilled.name && isFilled.email && isFilled.message ? (
           <button
